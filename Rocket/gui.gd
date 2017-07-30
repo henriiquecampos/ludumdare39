45d079b344321e6c8cf7901ef6399dfba8a112a5
Henrique Campos <henriiquecampos@gmail.com>
2017-07-29 23:58:01 -0300

    Modified game conditions
    Also added sprites

--- Rocket/gui.gd
+++ Rocket/gui.gd
@@ -1,2 +1,12 @@
 extends CanvasLayer
 
+onready var tapping = get_node("../TappingTimer")
+
+export (NodePath) var power_path
+onready var power = get_node(power_path)
+
+func _ready():
+	set_process(true)
+func _process(delta):
+	power.set_max(tapping.get_wait_time())
+	power.set_value(tapping.get_time_left())
--- Rocket/rocket.gd
+++ Rocket/rocket.gd
@@ -1,97 +1,82 @@
 extends RigidBody2D
 
-const MAX_VELOCITY = -400
-
-export (Vector2) var tapping_speed = Vector2(0, -200)
-export (float) var min_time = 0.5
+export (int, "NORMAL", "OPTIMAL", "FALLING") var ENGINE_STATE  = 0 setget set_engine_state
+export (float) var min_time = 1.0
 export (float) var max_time = 3.0
-
-export (NodePath) var tapping_path
-export (NodePath) var reaction_path
 export (NodePath) var camera_path
-onready var tapping = get_node(tapping_path)
-onready var reaction = get_node(reaction_path)
-onready var camera = get_node(camera_path)
+onready var tapping = get_node("TappingTimer")
 onready var sprite = get_node("Sprite")
-
-var needed_tappings = 1
-var tappings = 0
 var already_pressed = false
-var bonus_velocity = true
-var can_tap = false
-var velocity = Vector2(0, -400)
+var tapped = 0
+var needed_tappings = 5
+
 func _ready():
 	randomize()
+	get_node("CameraRig").set_remote_node("../%s" %camera_path)
 	set_fixed_process(true)
-	tapping.set_wait_time(_randomize_time())
-	tapping.start()
 	
 func _fixed_process(delta):
-	_set_camera()
-	if can_tap:
-		if Input.is_action_pressed("tapping") and not already_pressed:
-			if tappings >= needed_tappings:
-				if get_linear_velocity().y > MAX_VELOCITY:
-					if reaction.get_time_left() > 0:
-						velocity = Vector2(0, MAX_VELOCITY)
-					else:
-						velocity = tapping_speed
-						set_linear_velocity(get_linear_velocity() + velocity)
-					can_tap = false
-					get_node("Particles").set_emitting(true)
-			else:
-				tappings += 1
-	else:
-		if Input.is_action_pressed("tapping") and already_pressed:
-			sprite.get_node("Thrust").set_hidden(false)
-			set_linear_velocity(velocity)
-			get_node("Particles").set_emitting(false)
+	manage_inputs()
 	already_pressed = Input.is_action_pressed("tapping")
-
-func _on_tapping_timeout():
-	#Engine failing
-	flick_thrust()
-	_randomize_ignition()
-	get_node("Particles").set_emitting(false)
-	velocity = Vector2(0, 0)
-	can_tap = true
-	bonus_velocity = true
-	tapping.set_wait_time(_randomize_time())
-	tapping.start()
-	reaction.start()
-
-func _on_reaction_timeout():
-	bonus_velocity = false
-
-func _on_exit_screen():
-	get_tree().change_scene("res://Screens/GameOver.tscn")
 	
-func _set_camera():
-	if get_linear_velocity().normalized().y > 0:
-		get_node("CameraRig").set_remote_node(".")
-	else:
-		get_node("CameraRig").set_remote_node(camera.get_path())
+func set_engine_state(state):
+	if is_inside_tree():
+		if state == 0:
+			sprite.get_node("ThrustFailing").stop()
+			get_node("Particles").set_emitting(false)
+		elif state == 1:
+			flicking_thrust()
+			get_node("Particles").set_emitting(true)
+		elif state == 2:
+			sprite.get_node("Thrust").set_hidden(true)
+			sprite.get_node("ThrustFailing").stop()
+			get_node("Particles").set_emitting(true)
+	ENGINE_STATE = state
 
-func _on_body_enter( body ):
-	if body.is_in_group("arrival"):
-		get_node("Visibility").disconnect("exit_screen", self, "_on_exit_screen")
-		get_tree().change_scene("res://Screens/MainMenu.tscn")
-
-func _randomize_time():
-	return(rand_range(min_time, max_time))
-
-func _randomize_ignition():
-	needed_tappings = int(rand_range(5,11))
-	
-func flick_thrust():
-	get_node("Particles").set_emitting(true)
+func flicking_thrust():
 	sprite.get_node("ThrustFailing").set_wait_time(rand_range(0.1,0.3))
 	sprite.get_node("ThrustFailing").start()
-	if can_tap:
-		sprite.get_node("ThrustFailing").set_one_shot(true)
-		if sprite.get_node("Thrust").is_hidden():
-			sprite.get_node("Thrust").set_hidden(false)
-		else:
-			sprite.get_node("Thrust").set_hidden(true)
+
+func _on_thrust_fail():
+	if sprite.get_node("Thrust").is_hidden():
+		sprite.get_node("Thrust").set_hidden(false)
 	else:
-		sprite.get_node("ThrustFailing").set_one_shot(false)
+		sprite.get_node("Thrust").set_hidden(true)
+	flicking_thrust()
+
+func manage_inputs():
+	if ENGINE_STATE == 0:
+		if tapping.get_time_left() < tapping.get_wait_time() * 0.5:
+			set_engine_state(1)
+			flicking_thrust()
+		if Input.is_action_pressed("tapping") and already_pressed:
+			if abs(get_linear_velocity().y) < 400:
+				sprite.get_node("Thrust").set_hidden(false)
+				set_linear_velocity(get_linear_velocity() + Vector2(0, -100))
+		elif not Input.is_action_pressed("tapping") and already_pressed:
+			sprite.get_node("Thrust").set_hidden(true)
+				set_engine_state(2)
+				tapping.stop()
+	elif ENGINE_STATE == 1:
+		if tapping.get_time_left() < tapping.get_wait_time() * 0.25:
+			set_engine_state(2)
+			get_node("CameraRig").set_remote_node(".")
+			sprite.get_node("ThrustFailing").stop()
+		if Input.is_action_pressed("tapping") and not already_pressed:
+			set_linear_velocity(get_linear_velocity() + Vector2(0, -400))
+			restart_tapping()
+			set_engine_state(0)
+	elif ENGINE_STATE == 2:
+		if Input.is_action_pressed("tapping") and not already_pressed:
+			tapped += 1
+			if tapped >= needed_tappings:
+				set_engine_state(0)
+				restart_tapping()
+				set_linear_velocity(Vector2(0, 0))
+				get_node("CameraRig").set_remote_node("../%s" %camera_path)
+				get_node("InputDelay").start()
+				
+func restart_tapping():
+	tapping.set_wait_time(rand_range(min_time, max_time))
+	tapping.start()
+
